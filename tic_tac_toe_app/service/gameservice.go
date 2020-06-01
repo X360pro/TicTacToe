718c9541926846b495d1226eb5e9063be87bdeca
X360pro <43405739+X360pro@users.noreply.github.com>
2020-06-01 15:10:46 +0530

    Update gameservice.go

--- tic_tac_toe_app/service/gameservice.go
+++ tic_tac_toe_app/service/gameservice.go
@@ -1,40 +1,40 @@
 package service
 
 import (
-	"example/tic_tac_toe_app/components"
-	"fmt"
+	"components"
+	"errors"
 )
 
 type GameService struct {
 	*ResultService
-	players [2]*components.Player
+	player [2]*components.Player
 }
 
-var turn int = 0
-
-func NewGameService(rs *ResultService, players [2]*components.Player) *GameService {
-	return &GameService{rs, players}
-
+func NewGameService(pl [2]*components.Player, size int) *GameService {
+	return &GameService{NewResultService(NewBoardService(size)), pl}
 }
 
-func (gs *GameService) Play(pos uint8) (Result, error) {
-	if pos < 0 || pos >= gs.Size*gs.Size {
-		return Result{"", false, false}, fmt.Errorf("please enter integer in range (0,%d)", gs.Size*gs.Size-1)
+var turn = 0
+
+func (g *GameService) Play(pos uint8) (error, string) {
+	if pos < 0 || pos > g.Size*g.Size-1 {
+		return errors.New("Positoin is out of bounds"), "nil"
 	}
-	var res Result
+	var res string
 	if turn%2 == 0 {
-		err := gs.PutMarkInPosition(gs.players[0], pos)
+		err := g.PutMarkInPosition(pos, g.player[0].Mark)
 		if err != nil {
-			return Result{"", false, false}, err
+			return err, "nil"
 		}
-		res = gs.GetResult(gs.players[0], pos)
-	} else if turn%2 == 1 {
-		err := gs.PutMarkInPosition(gs.players[1], pos)
+		res = g.GiveResult(g.player[0])
+	} else {
+		err := g.PutMarkInPosition(pos, g.player[1].Mark)
 		if err != nil {
-			return Result{"", false, false}, err
+			return err, "nil"
 		}
-		res = gs.GetResult(gs.players[1], pos)
+		res = g.GiveResult(g.player[1])
 	}
 	turn++
-	return res, nil
+	return nil, res
+
 }
